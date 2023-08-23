bac4e320fbcf513d07593c0f22ba2091df6265ec
Christopher Perez <christopher.perez@north-47.com>
2023-08-23 12:05:57 +0200

    first commit

---
+++ components/Nav.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col">
+    <div v-for="route in router.getRoutes()" :key="route.path">
+      <nuxt-link :to="route.path">{{ route.name }}</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const router = useRouter()
+</script>
